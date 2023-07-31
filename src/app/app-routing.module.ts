4ddd1ef339dceeb43a824b8a9450ce69917874e5
Lebohang <lebohang.mhlane@xgileit.com>
2023-07-31 16:27:18 +0200

    reset

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ToolbarComponent } from './toolbar/toolbar.component';
 
 const routes: Routes = [
-  { path: "profile", component: ToolbarComponent },
 
 ];
 
