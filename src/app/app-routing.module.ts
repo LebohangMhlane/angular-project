2c915252c15c52f297cddaef6176273ce9981453
Lebohang <lebohang.mhlane@xgileit.com>
2023-08-05 17:11:53 +0200

    bootstrap

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { MainComponent } from './main/main.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
 
 const routes: Routes = [
-
+  {path: "home", component: MainComponent},
+  {path: "register", component: RegistrationpageComponent},
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { MainComponent } from './main/main.component';
+import { TopbarComponent } from './topbar/topbar.component';
+import { RegistrationpageComponent } from './registrationpage/registrationpage.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    MainComponent,
+    TopbarComponent,
+    RegistrationpageComponent,
   ],
   imports: [
     BrowserModule,
