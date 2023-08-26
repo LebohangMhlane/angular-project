2978bc5c1fe1004e925530d5a0f61517882059c8
Lebohang <lebohang.mhlane@xgileit.com>
2023-08-26 16:25:09 +0200

    worked on profile css component

--- src/app/myprofilepage/myprofilepage.component.ts
+++ src/app/myprofilepage/myprofilepage.component.ts
@@ -7,7 +7,8 @@ import { Component } from '@angular/core';
     './myprofilepage.component.css',
     './myprofilepage.css.components/messages.css.component/messages.css',
     './myprofilepage.css.components/offers.css.component/offers.css',
-    './myprofilepage.css.components/contracthistory.css.component/contracthistory.css'
+    './myprofilepage.css.components/contracthistory.css.component/contracthistory.css',
+    './myprofilepage.css.components/profile.css.component/profile.css'
   ]
 })
 export class MyprofilepageComponent {
