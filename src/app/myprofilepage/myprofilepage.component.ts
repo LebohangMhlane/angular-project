d25208ec59dbd99570e54bc3c4328b6bbffcdfce
Lebohang <lebohang.mhlane@xgileit.com>
2023-08-26 15:36:16 +0200

    made components out of everything

--- src/app/myprofilepage/myprofilepage.component.ts
+++ src/app/myprofilepage/myprofilepage.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-myprofilepage',
   templateUrl: './myprofilepage.component.html',
-  styleUrls: ['./myprofilepage.component.css']
+  styleUrls: [
+    './myprofilepage.component.css',
+    './myprofilepage.css.components/messages.css.component/messages.css',
+    './myprofilepage.css.components/offers.css.component/offers.css',
+    './myprofilepage.css.components/contracthistory.css.component/contracthistory.css'
+  ]
 })
 export class MyprofilepageComponent {
 
--- src/app/topbar/topbar.component.ts
+++ src/app/topbar/topbar.component.ts
@@ -3,7 +3,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-topbar',
   templateUrl: './topbar.component.html',
-  styleUrls: ['./topbar.component.css']
+  styleUrls: [
+    './topbar.component.css',
+    './topbar.css.components/navbar.css.component/navbar.css',
+    './topbar.css.components/topbar.css.component/topbar.css',
+
+  ]
 })
 export class TopbarComponent {
 
