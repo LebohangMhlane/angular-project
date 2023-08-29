641cf7983fa9778e1e5dc702d14179a6e00c0996
Lebohang <lebohang.mhlane@xgileit.com>
2023-08-29 13:12:56 +0200

    current updates

--- src/app/myprofilepage/myprofilepage.component.ts
+++ src/app/myprofilepage/myprofilepage.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-myprofilepage',
@@ -8,9 +8,21 @@ import { Component } from '@angular/core';
     './myprofilepage.css.components/messages.css.component/messages.css',
     './myprofilepage.css.components/offers.css.component/offers.css',
     './myprofilepage.css.components/contracthistory.css.component/contracthistory.css',
-    './myprofilepage.css.components/profile.css.component/profile.css'
+    './myprofilepage.css.components/contracthistory.css.component/profile.css'
   ]
 })
 export class MyprofilepageComponent {
+
+  constructor(private renderer: Renderer2, private elementRef: ElementRef){
+  };
+
+  contracts: number[] = [1, 2, 3, 4, 5, 6, 7];
+  maximumReached: boolean = false;
+
+  printList(){
+    this.contracts.forEach(element => {
+      console.log(element);
+    });
+  }
 
 }
