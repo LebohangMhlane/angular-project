3f74762164d0702391e4309756c5f5fc565cb400
LebohangMhlane <75252307+LebohangMhlane@users.noreply.github.com>
2024-02-23 10:27:49 +0200

    cleaned up home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,41 +8,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-  httpService;
-  jsonResponse: any;
-
-  constructor(httpService: HttpClient){
-    this.httpService = httpService;
-  }
-
-  loadingImage : boolean = false;
-  currentIndex : number = 0;
-  dogImages = ["https://images.dog.ceo/breeds/terrier-dandie/n02096437_3167.jpg"];
-  
-  async getDogImage() {
-    try{
-      this.loadingImage = true;
-      let newIndex : number = this.currentIndex + 1;
-      if(newIndex > this.dogImages.length - 1){
-        let url = "https://dog.ceo/api/breeds/image/random";
-        this.httpService.get(url).subscribe(data => {
-          this.jsonResponse = data as Record<string, string>;
-          this.dogImages.push(this.jsonResponse["message"]);
-          this.currentIndex = newIndex;
-          this.loadingImage = false;
-        });
-      } else {
-        this.loadingImage = false;
-        this.currentIndex = newIndex;
-      }
-    } catch (error) {
-      this.loadingImage = false;
-      console.log("Failed to get image from api");
-    }
-  }
-
-  getPreviousDogImage(){
-    this.currentIndex = this.currentIndex - 1;
-  }
 
 }
